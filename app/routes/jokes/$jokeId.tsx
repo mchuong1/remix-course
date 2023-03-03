6cc987760023e069f439993b12705aabad63f95f
Matthew Chuong <mstchuong@yahoo.com>
2023-03-02 21:03:38 -0500

    Add SEO to our project

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,8 +1,23 @@
-import type { LoaderArgs, ActionArgs } from "@remix-run/node";
+import type { LoaderArgs, ActionArgs, MetaFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { useLoaderData, useParams, useCatch } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 import { requireUserId, getUserId } from "~/utils/session.server";
+
+export const meta: MetaFunction<typeof loader> = ({
+  data,
+}) => {
+  if (!data) {
+    return {
+      title: "No joke",
+      description: "No joke found",
+    };
+  }
+  return {
+    title: `"${data.joke.name}" joke`,
+    description: `Enjoy the "${data.joke.name}" joke and much more`,
+  };
+};
 
 export const loader = async ({ params, request }: LoaderArgs) => {
   const { jokeId } = params;
