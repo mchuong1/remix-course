b1b3657a52a637d04879564cf315a1c784d8db48
Matthew Chuong <mstchuong@yahoo.com>
2023-03-02 21:09:54 -0500

    load javascript

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import {
   Outlet,
   useCatch,
   Meta,
+  Scripts,
 } from "@remix-run/react";
 
 import globalStylesUrl from "./styles/global.css";
@@ -61,6 +62,7 @@ function Document({
       </head>
       <body>
         {children}
+        <Scripts />
         <LiveReload />
       </body>
     </html>
@@ -92,6 +94,7 @@ export function CatchBoundary() {
 }
 
 export function ErrorBoundary({ error }: { error: Error }) {
+
   return (
     <Document title="Uh-oh!">
       <div className="error-container">
